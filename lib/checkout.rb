893724190a954e09ca5e3d4c202b4b2a0c2cc3d4
Student <student@Admins-MacBook-Air.local>
2019-10-22 10:11:57 +0100

    added several tests oops

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,2 +1,7 @@
 class Checkout
+  attr_reader :item_price
+
+  def initialize
+    @item_price = 1
+  end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,9 +1,15 @@
 require "checkout"
 
 describe "checkout" do
-  let(:item) { Checkout.new }
+  let(:checkout) { Checkout.new }
 
   it "can create instances of Checkout" do
-    expect(item).to be_instance_of(Checkout)
+    expect(checkout).to be_instance_of(Checkout)
+  end
+  it "has an item priced at 1" do
+    expect(checkout.item_price).to eq 1
+  end
+  it "lets the customer know the price of the item" do
+    expect(checkout.price_checker).to eq 1
   end
 end
